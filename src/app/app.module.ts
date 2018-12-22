af0639d59cd3187e59f1c3129112645bc84c7555
eternally_glb <1306513880@qq.com>
2018-12-22 17:13:03 +0800

    增加了aboutme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { HeadComponent } from './head/head.component';
+import { FooterComponent } from './footer/footer.component';
+import { IndexComponent } from './content/rightsides/index/index.component';
+import { AlbumComponent } from './content/album/album.component';
+import { LeavemessageComponent } from './content/rightsides/leavemessage/leavemessage.component';
+import { LeftsideComponent } from './content/leftsides/leftside/leftside.component';
+import { BlogcontentComponent } from './content/rightsides/blogcontent/blogcontent.component';
+import { AboutmeComponent } from './content/rightsides/aboutme/aboutme.component';
 
+
+const appRoutes: Routes = [
+  { path: 'index', component: IndexComponent },
+  { path: 'aboutme', component: AboutmeComponent },
+  { path: 'blog/:id', component: BlogcontentComponent },
+  { path: 'album', component: AlbumComponent },
+  { path: 'leavemessage', component: LeavemessageComponent },
+  { path: '**', component: IndexComponent }
+
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeadComponent,
+    FooterComponent,
+    IndexComponent,
+    AlbumComponent,
+    LeavemessageComponent,
+    LeftsideComponent,
+    BlogcontentComponent,
+    AboutmeComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }
+    ),
     BrowserModule
   ],
   providers: [],
